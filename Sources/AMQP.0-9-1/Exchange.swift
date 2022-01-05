4f8e9dc6ccf51ce7052a9062bb0326fea74e99a5
Matt Manzi <imyourmanzi1@gmail.com>
2022-01-05 16:13:23 -0500

    Add init to determine exchange type

--- Sources/AMQP.0-9-1/Exchange.swift
+++ Sources/AMQP.0-9-1/Exchange.swift
@@ -9,5 +9,24 @@ public enum ExchangeType: String {
  The entity within the server which receives messages from producer applications and optionally routes these to message queues within the server.
  */
 internal struct Exchange {
-    
+    internal private(set) var bindings: [Binding]
+    private(set) var type: ExchangeType
+
+    init?(type: String?) {
+        bindings = []
+
+        switch type {
+        case nil:
+            fallthrough
+        case "":
+            self.type = .Direct
+        case "amq.headers":
+            self.type = .Headers
+        default:
+            guard let t = ExchangeType(rawValue: type!) else {
+                return nil
+            }
+            self.type = t
+        }
+    }
 }

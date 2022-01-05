b1fbe30ade878f5fcca62c02634bcbd886790dc6
Matt Manzi <imyourmanzi1@gmail.com>
2022-01-05 16:06:08 -0500

    Add formatter

--- Package.swift
+++ Package.swift
@@ -9,20 +9,23 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AMQP.0-9-1",
-            targets: ["AMQP.0-9-1"]),
+            targets: ["AMQP.0-9-1"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.49.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AMQP.0-9-1",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "AMQP.0-9-1Tests",
-            dependencies: ["AMQP.0-9-1"]),
+            dependencies: ["AMQP.0-9-1"]
+        ),
     ]
 )
